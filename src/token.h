ad278abcfddfcd91c200b0f878fe37097f9721f0
StanislavMechl <smechl@seznam.cz>
2017-10-19 10:22:53 +0200

    Add error states in token

--- src/token.h
+++ src/token.h
@@ -20,6 +20,7 @@
     typedef enum
     {
         token_blank,
+        token_error,
         token_identifier,
         token_integer,
         token_double,
@@ -30,17 +31,28 @@
         // ... bude ich asi urcite viac, neviem ale ake vsetky chceme,
     } token_type;
 
+    typedef enum
+    {
+        err_lex, // 1 - chybna struktura aktualniho lexemu
+        err_syn, // 2 - chybna syntaxe
+        err_sem_def, // 3 - nedefinova funkce/promena nebo pokus o redefinici
+        err_sem_com, // 4 - nekompatibilni typy nebo spatne parametry funkce
+        err_sem_oth, // 6 - ostatni semanticke chyby
+        err_int, // 99 - interni chyba prekladace (neovlivnena vstupnim programem)
+    } error_type;
+
     typedef union
     {
         char *c;
         double d;
         int i;
+        error_type e;
     } attribute_value;
 
     // datovy typ pro objekt 'token'
     // pokud mame ukazatel pT ukazujici na token,
     // k typu pristoupime jako pT->type
-    // a k atributu jako pT->att.c  (pripadne i nebo d)
+    // a k atributu jako pT->att.c  (pripadne i, d nebo e)
     // pozor! c je ukazatel! pro string ok pro pristup ke znaku *(pT->att.c)
     typedef struct _TOKEN
     {
