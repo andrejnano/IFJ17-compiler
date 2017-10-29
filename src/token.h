c2a837f1f325d31f152323d10894f9aabe9e792e
jansv <honza-svanda@seznam.cz>
2017-10-29 21:11:38 +0100

    Update token.h
    added token_val* for integer, double and string to differentiate from keywords integer, double and string

--- src/token.h
+++ src/token.h
@@ -3,7 +3,6 @@
     | PROJEKT IFJ17  |
     + -------------- +
     Implementace prekladace imperativního jazyka IFJ17.
-
     Autori:
         xmarko15 Peter Marko
         xmechl00 Stanislav Mechl
@@ -19,12 +18,15 @@
     // Token type, identifies token
     typedef enum
     {
+
+    	// Data types
         token_blank,
         token_identifier,
-        token_integer,
-        token_double,
-        token_string,
-        
+        token_val_integer,
+        token_val_double,
+        token_val_string,
+
+        // Operators
         token_op_add,
         token_op_sub,
         token_op_mul,
@@ -36,54 +38,54 @@
         token_op_ge,
         token_op_eq,
         token_op_ne,
-        
+
+        // Special
         token_semicolon,
         token_comma,
         token_lbrace,
         token_rbrace,
-        
+
+        // Keywords
+        token_and,
         token_as,
         token_asc,
+        token_boolean,
+        token_chr,
+        token_continue,
         token_declare,
         token_dim,
         token_do,
         token_double,
         token_else,
+        token_elseif,
         token_end,
-        token_chr,
+        token_exit,
+        token_false,
+        token_for,
         token_function,
         token_if,
         token_input,
         token_integer,
         token_length,
         token_loop,
-        token_print,
-        token_return,
-        token_scope,
-        token_string,
-        token_substr,
-        token_then,
-        token_while,
-        
-        /*
-        token_and,
-        token_boolean,
-        token_continue,
-        token_elseif,
-        token_exit,
-        token_false,
-        token_for,
         token_next,
         token_not,
         token_or,
+        token_print,
+        token_return,
+        token_scope,
         token_shared,
         token_static,
+        token_string,
+        token_substr,
+        token_then,
         token_true,
-        */
-        
+        token_while,
+
+        // Read state
         token_eol,
         token_eof,
-        
+
     } token_type;
 
     // Token values
@@ -102,14 +104,13 @@
     typedef struct _TOKEN
     {
         token_type type;
-        token_value value;
-        unsigned int line;
+        token_value value; // Only if token type is valid with value
+        unsigned int line; // On which line is the token located
     } Token_t;
 
     /*
         Token_t *mytoken;
         mytoken = get_token(file);
-
         mytoken->type = token_identifier;
         mytoken->value.i = 1234;
         mytoken->line = 3; // <1, inf)
